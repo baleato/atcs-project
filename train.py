052843c511ae1fb5c67711211ee1603ba24d2e49
Daniel Baleato <baleato@gmail.com>
2020-04-26 10:15:49 +0200

    Add tensorboard logging

--- train.py
+++ train.py
@@ -12,12 +12,14 @@ from datetime import datetime
 import torch.optim as optim
 
 
-def train(iter, model):
+def train(iter, model, args):
     # Define optimizers and loss function
     optimizer = optim.Adam(params=model.parameters(), lr=0.00002)
     criterion = nn.BCEWithLogitsLoss()
 
+    writer = SummaryWriter(os.path.join(args.save_path, 'runs', '{}'.format(datetime.now())))
     # Iterate over the data
+    iterations, running_loss = 0, 0.0
     for epoch in range(10):
         epoch_loss = []
         for i, batch in enumerate(iter):
@@ -30,6 +32,7 @@ def train(iter, model):
             labels = batch[1]
 
             # Feed sentences into BERT instance, compute loss, perform backward pass, update weights.
+            iterations += 1
             predictions = model(sentences)[0]
             labels = labels.type_as(predictions)
             loss = criterion(predictions, labels)
@@ -37,7 +40,14 @@ def train(iter, model):
             optimizer.step()
 
             epoch_loss.append(loss.item())
+
+            running_loss += loss.item()
+            if iterations % args.log_every == 0:
+                writer.add_scalar('training loss', running_loss / args.log_every, iterations)
+                running_loss = 0.0
             print(loss.item())
+    writer.close()
+
 
 if __name__ == '__main__':
     args = get_args()
@@ -53,4 +63,4 @@ if __name__ == '__main__':
     model = load_model()
     model = model.to(device)
 
-    results = train(train_iter, model)
+    results = train(train_iter, model, args)
