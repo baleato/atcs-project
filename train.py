d17b4cc981c04220885632510ade4b95b7225d29
Daniel Baleato <baleato@gmail.com>
2020-04-27 16:55:37 +0200

    Validate test set and improve logging

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import os
 import time
 import sys
 import glob
+from datetime import timedelta
 
 from sklearn.metrics import jaccard_score
 from torch import load
@@ -18,11 +19,22 @@ from datetime import datetime
 import torch.optim as optim
 
 
-def train(model, args):
+def evaluate_emo(outputs, gold_labels):
+    threshold = 0.5
+    pred_labels = (outputs.clone().detach() > threshold).type_as(gold_labels)
+    micro = jaccard_score(pred_labels, gold_labels, average='micro')
+    macro = jaccard_score(pred_labels, gold_labels, average='macro')
+    return micro, macro
+
+
+def train(model, args, device):
     print("Creating DataLoaders")
     train_iter = create_iters(path='./data/semeval18_task1_class/train.txt',
                               order='random',
                               batch_size=args.batch_size)
+    dev_iter = create_iters(path='./data/semeval18_task1_class/dev.txt',
+                            order='random',
+                            batch_size=args.batch_size)
 
     # Define optimizers and loss function
     optimizer = optim.Adam(params=model.parameters(), lr=args.lr)
@@ -32,15 +44,20 @@ def train(model, args):
     os.makedirs(args.save_path, exist_ok=True)
     writer = SummaryWriter(
         os.path.join(args.save_path, 'runs', '{}'.format(datetime.now())))
-    header = '      Loss      Micro      Macro'
-    log_template = '{:10.6f} {:10.6f} {:10.6f}'
+    header = '      Time   Epoch  Iteration   Progress  %Epoch       ' + \
+        'Loss   Dev/Loss      Micro    Dev/Micro      Macro    Dev/Macro'
+    log_template = '{:>10} {:7.0f} {:10.0f} {:5.0f}/{:<5.0f} {:5.0f}% ' + \
+        '{:10.6f}            {:10.6f}              {:10.6f}'
+    dev_log_template = '{:>10} {:7.0f} {:10.0f} {:5.0f}/{:<5.0f} {:6.0f}' + \
+        '            {:10.6f}            {:12.6f}            {:12.6f}'
     print(header)
+    start = time.time()
 
     # Iterate over the data
     iterations, running_loss = 0, 0.0
     for epoch in range(args.max_epochs):
         model.train()
-        for batch in train_iter:
+        for batch_idx, batch in enumerate(train_iter):
             # Reset .grad attributes for weights
             optimizer.zero_grad()
 
@@ -56,25 +73,27 @@ def train(model, args):
             loss.backward()
             optimizer.step()
 
-            # Compute accuracy
-            threshold = 0.5
-            pred_labels = (
-                predictions.clone().detach() > threshold).type_as(labels)
-            emo_micro = jaccard_score(pred_labels, labels, average='micro')
-            emo_macro = jaccard_score(pred_labels, labels, average='macro')
-
             running_loss += loss.item()
             iterations += 1
             if iterations % args.log_every == 0:
-                writer.add_scalar(
-                    'training loss',
-                    running_loss / args.log_every, iterations)
+                emo_micro, emo_macro = evaluate_emo(predictions, labels)
+                iter_loss = running_loss / args.log_every
+                writer.add_scalar('training loss', iter_loss, iterations)
+                writer.add_scalar('training micro', emo_micro, iterations)
+                writer.add_scalar('training macro', emo_macro, iterations)
+                print(log_template.format(
+                    str(timedelta(seconds=int(time.time() - start))),
+                    epoch,
+                    iterations,
+                    batch_idx+1, len(train_iter),
+                    (batch_idx+1) / len(train_iter) * 100,
+                    iter_loss, emo_micro, emo_macro))
                 running_loss = 0.0
-            print(log_template.format(loss.item(), emo_micro, emo_macro))
 
             # saving redundant parameters
             # Save model checkpoints.
             if iterations % args.save_every == 0:
+                emo_micro, emo_macro = evaluate_emo(predictions, labels)
                 snapshot_prefix = os.path.join(args.save_path, 'snapshot')
                 snapshot_path = snapshot_prefix + \
                     '_micro_{:.4f}_macro_{:.4f}_loss_{:.6f}_iter_{}_model.pt' \
@@ -85,10 +104,40 @@ def train(model, args):
                     if f != snapshot_path:
                         os.remove(f)
 
-        # TODO:
-        # - Evaluate model on dev set
-        # - Store model with best performing on dev set
-        # - Log dev set results
+        # ============================ EVALUATION ============================
+        model.eval()
+
+        # calculate accuracy on validation set
+        sum_dev_loss, sum_micro, sum_macro = 0, 0, 0
+        with torch.no_grad():
+            for dev_batch in dev_iter:
+                sentences = dev_batch[0].to(device)
+                labels = dev_batch[1]
+                outputs = model(sentences)
+                # Loss
+                batch_dev_loss = criterion(outputs, labels.type_as(outputs))
+                sum_dev_loss += batch_dev_loss.item()
+                # Accuracy
+                emo_micro, emo_macro = evaluate_emo(outputs, labels)
+                sum_micro += emo_micro
+                sum_macro += emo_macro
+        dev_loss = sum_dev_loss / len(dev_iter)
+        dev_micro = sum_micro / len(dev_iter)
+        dev_macro = sum_macro / len(dev_iter)
+
+        print(dev_log_template.format(
+                str(timedelta(seconds=int(time.time() - start))),
+                epoch,
+                iterations,
+                batch_idx+1, len(train_iter),
+                (batch_idx+1) / len(train_iter) * 100,
+                dev_loss, dev_micro, dev_macro))
+
+        writer.add_scalar('dev loss', dev_loss, iterations)
+        writer.add_scalar('dev micro', dev_micro, iterations)
+        writer.add_scalar('dev macro', dev_macro, iterations)
+
+        # TODO: Store model with best performing on dev set
 
     writer.close()
 
@@ -106,4 +155,4 @@ if __name__ == '__main__':
         model = MetaLearner(args)
         model.to(device)
 
-    results = train(model, args)
+    results = train(model, args, device)
