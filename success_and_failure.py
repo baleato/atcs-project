466841def246bb113f647ebda33281ef7d64ee8a
ChristophHoenes <christoph.hoenes@gmail.com>
2020-05-23 23:06:33 +0200

    add file

---
+++ success_and_failure.py
@@ -0,0 +1,10 @@
+import torch
+import numpy as np
+import pandas as pd
+
+
+def get_success_and_failure_cases():
+    pass
+
+if __name__ == '__main__':
+    difference_list = get_success_and_failure_cases()
