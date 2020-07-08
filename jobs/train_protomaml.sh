393c995d46fc5997086cddc722ab8d6cf7e4bee3
Daniel Baleato <baleato@gmail.com>
2020-07-08 23:25:20 +0200

    Unfreeze all BERT layers

--- jobs/train_protomaml.sh
+++ jobs/train_protomaml.sh
@@ -27,5 +27,5 @@ pip install --user -r requirements.txt
 
 srun python meta_train.py --mlp_dims 768 --batch_size 16 --meta_batch_size 32 --eval_every 100 --lr 1e-4 --inner_lr 1e-3 --num_iterations 3000 \
 	--training_tasks  IronySubtaskA Politeness SarcasmDetection SemEval18 SentimentAnalysis --validation_task Offenseval \
-	--episodes data/episodes/episodes_Offenseval_k_8_episodes_32.pkl \
-	--save_path ~/results_ProtoMAML_Offenseval_mpl768_batch16_metabatch32_lr1e-4_ilr1e-3_numiter3000
+	--episodes data/episodes/episodes_Offenseval_k_8_episodes_32.pkl --unfreeze_num 12 \
+	--save_path ~/results_ProtoMAML_Offenseval_mpl768_batch16_metabatch32_lr1e-4_ilr1e-3_unfreeze12_numiter3000
